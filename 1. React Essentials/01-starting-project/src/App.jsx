d0fdf2ae74f9f6deea5a78b7b29d3b16f34ec795
Duy Le <kmktag904@gmail.com>
2024-11-18 21:19:23 -0500

    react essential done

--- 1. React Essentials/01-starting-project/src/App.jsx
+++ 1. React Essentials/01-starting-project/src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
+import { EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import TabButton from './components/TabButton/TabButton.jsx';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -16,33 +26,48 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
             it will automatically pass in the prop */}
-            <CoreConcept 
-              title={CORE_CONCEPTS[0].title} 
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image} 
-            />
-            <CoreConcept 
-              title={CORE_CONCEPTS[1].title} 
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image} 
-            />
-            <CoreConcept 
-              title={CORE_CONCEPTS[2].title} 
-              description={CORE_CONCEPTS[2].description}
-              image={CORE_CONCEPTS[2].image} 
-            />
-            <CoreConcept 
-              title={CORE_CONCEPTS[3].title} 
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image} 
-            />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem}/>
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Something here</TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'components'} 
+              onSelect={() => handleSelect('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'jsx'} 
+              onSelect={() => handleSelect('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'props'} 
+              onSelect={() => handleSelect('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton 
+              isSelected={selectedTopic === 'state'} 
+              onSelect={() => handleSelect('state')}
+            >
+              State
+            </TabButton>
           </menu>
+          {!selectedTopic ? (<p>Please select a topic.</p>) : (<div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>)}
         </section>
       </main>
     </div>
--- 1. React Essentials/01-starting-project/src/components/TabButton/TabButton.jsx
+++ 1. React Essentials/01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,10 +1,10 @@
-export default function TabButton({children}) {
+export default function TabButton({children, onSelect, isSelected}) {
     // in order to get the text between the components
     // we use special prop name "children"
     // it's called component composition
     return (
         <li>
-            <button>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
         </li>
     );
 }
